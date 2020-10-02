492f507ee782f9389837f224ff8deed031241a62
mcseexn <mcseexn@163.com>
2020-10-02 23:54:13 +0800

    test

--- hello-spring-boot/src/main/java/com/example/test/TestJava.java
+++ hello-spring-boot/src/main/java/com/example/test/TestJava.java
@@ -1,4 +1,17 @@
 package com.example.test;
 
 public class TestJava {
+
+    private static int age = 10;
+    static String name = "lsx";
+
+    public static void main(String[] args) {
+
+        if ("lsx".equals(name)) {
+            age = 11;
+        } else {
+            age = 12;
+        }
+        System.out.println("年龄为："+age);
+    }
 }
